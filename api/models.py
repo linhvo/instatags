0585a83dba3f4ece46cb37a401426bca57821a63
Trung Vu <trungduyvu@gmail.com>
2018-04-10 23:22:21 -0500

    instagram authentication

---
+++ api/models.py
@@ -0,0 +1,13 @@
+
+from django.db import models
+
+
+class InstagramUser(models.Model):
+    access_token = models.CharField(max_length=255)
+    username = models.CharField(max_length=255, unique=True)
+    bio = models.CharField(max_length=255, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    profile_picture = models.URLField(blank=True, null=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    instagram_id = models.IntegerField()
+
---
+++ api/views.py
@@ -0,0 +1,58 @@
+import json
+import os
+
+import requests
+from instagram import client
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST
+
+from api.models import InstagramUser
+
+CONFIG = {
+    'client_id': os.environ.get('CLIENT_ID'),
+    'client_secret': os.environ.get('CLIENT_SECRET'),
+    'redirect_uri': 'http://127.0.0.1:8000/oauth_callback'
+}
+
+unauthenticated_api = client.InstagramAPI(**CONFIG)
+
+
+@api_view(['GET'])
+def home(request):
+    try:
+        url = unauthenticated_api.get_authorize_url(scope=["likes", "comments"])
+        return Response(url)
+    except Exception as e:
+        print(e)
+
+
+@api_view(['GET'])
+def on_callback(request):
+    code = request.GET.get("code")
+    data = None
+    if not code:
+        return Response('Missing code')
+    try:
+        url = 'https://api.instagram.com/oauth/access_token'
+        data = {
+            'client_id': CONFIG['client_id'],
+            'client_secret': CONFIG['client_secret'],
+            'code': code,
+            'grant_type': 'authorization_code',
+            'redirect_uri': CONFIG['redirect_uri']
+        }
+
+        response = requests.post(url, data=data)
+        data = json.loads(response.content)
+        InstagramUser.objects.get_or_create(access_toke=data['access_token'],
+                                            username=data['user']['username'],
+                                            bio=data['user']['bio'],
+                                            website=data['user']['website'],
+                                            profile_picture=data['user']['profile_picture'],
+                                            full_name=data['user']['full_name'],
+                                            instagram_id=data['user']['id'])
+
+    except Exception as e:
+        Response(status=HTTP_400_BAD_REQUEST, data="Error: %s" % e)
+    return Response(data)
